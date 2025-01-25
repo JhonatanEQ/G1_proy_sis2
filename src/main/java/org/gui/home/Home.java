c4bad0143391bd156fc81b54de373c21ceb2c32d
JhontanEQ <encinas.jhonatan.dev@gmail.com>
2025-01-25 14:59:48 -0400

    FEAT: Create dashboard interface desing

--- src/main/java/org/gui/home/Home.java
+++ src/main/java/org/gui/home/Home.java
@@ -26,18 +26,219 @@ public class Home extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jpMenu = new javax.swing.JPanel();
+        jlTitle = new javax.swing.JLabel();
+        jSeparator3 = new javax.swing.JSeparator();
+        jpDashboard = new javax.swing.JPanel();
+        jpInventory = new javax.swing.JPanel();
+        jpSale = new javax.swing.JPanel();
+        jpReports = new javax.swing.JPanel();
+        jpInvoices = new javax.swing.JPanel();
+        jpAlerts = new javax.swing.JPanel();
+        jpSettings = new javax.swing.JPanel();
+        jSeparator1 = new javax.swing.JSeparator();
+        jpView = new javax.swing.JPanel();
+        jlTitleD = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jPanel4 = new javax.swing.JPanel();
+        jPanel5 = new javax.swing.JPanel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(153, 153, 153));
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jpMenu.setBackground(new java.awt.Color(255, 255, 255));
+        jpMenu.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jlTitle.setFont(new java.awt.Font("Roboto Black", 0, 14)); // NOI18N
+        jlTitle.setText("TechPoint Manager");
+        jpMenu.add(jlTitle, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 6, 138, 24));
+        jlTitle.getAccessibleContext().setAccessibleName("jlTitle");
+
+        jpMenu.add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 40, 150, 20));
+
+        javax.swing.GroupLayout jpDashboardLayout = new javax.swing.GroupLayout(jpDashboard);
+        jpDashboard.setLayout(jpDashboardLayout);
+        jpDashboardLayout.setHorizontalGroup(
+            jpDashboardLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 150, Short.MAX_VALUE)
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+        jpDashboardLayout.setVerticalGroup(
+            jpDashboardLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 40, Short.MAX_VALUE)
         );
+
+        jpMenu.add(jpDashboard, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 60, 150, 40));
+
+        javax.swing.GroupLayout jpInventoryLayout = new javax.swing.GroupLayout(jpInventory);
+        jpInventory.setLayout(jpInventoryLayout);
+        jpInventoryLayout.setHorizontalGroup(
+            jpInventoryLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 150, Short.MAX_VALUE)
+        );
+        jpInventoryLayout.setVerticalGroup(
+            jpInventoryLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 40, Short.MAX_VALUE)
+        );
+
+        jpMenu.add(jpInventory, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 120, 150, 40));
+
+        javax.swing.GroupLayout jpSaleLayout = new javax.swing.GroupLayout(jpSale);
+        jpSale.setLayout(jpSaleLayout);
+        jpSaleLayout.setHorizontalGroup(
+            jpSaleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 150, Short.MAX_VALUE)
+        );
+        jpSaleLayout.setVerticalGroup(
+            jpSaleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 40, Short.MAX_VALUE)
+        );
+
+        jpMenu.add(jpSale, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 180, 150, 40));
+
+        javax.swing.GroupLayout jpReportsLayout = new javax.swing.GroupLayout(jpReports);
+        jpReports.setLayout(jpReportsLayout);
+        jpReportsLayout.setHorizontalGroup(
+            jpReportsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 150, Short.MAX_VALUE)
+        );
+        jpReportsLayout.setVerticalGroup(
+            jpReportsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 40, Short.MAX_VALUE)
+        );
+
+        jpMenu.add(jpReports, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 240, 150, 40));
+
+        javax.swing.GroupLayout jpInvoicesLayout = new javax.swing.GroupLayout(jpInvoices);
+        jpInvoices.setLayout(jpInvoicesLayout);
+        jpInvoicesLayout.setHorizontalGroup(
+            jpInvoicesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 150, Short.MAX_VALUE)
+        );
+        jpInvoicesLayout.setVerticalGroup(
+            jpInvoicesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 40, Short.MAX_VALUE)
+        );
+
+        jpMenu.add(jpInvoices, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 300, 150, 40));
+
+        javax.swing.GroupLayout jpAlertsLayout = new javax.swing.GroupLayout(jpAlerts);
+        jpAlerts.setLayout(jpAlertsLayout);
+        jpAlertsLayout.setHorizontalGroup(
+            jpAlertsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 150, Short.MAX_VALUE)
+        );
+        jpAlertsLayout.setVerticalGroup(
+            jpAlertsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 40, Short.MAX_VALUE)
+        );
+
+        jpMenu.add(jpAlerts, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 360, 150, 40));
+
+        javax.swing.GroupLayout jpSettingsLayout = new javax.swing.GroupLayout(jpSettings);
+        jpSettings.setLayout(jpSettingsLayout);
+        jpSettingsLayout.setHorizontalGroup(
+            jpSettingsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 150, Short.MAX_VALUE)
+        );
+        jpSettingsLayout.setVerticalGroup(
+            jpSettingsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 40, Short.MAX_VALUE)
+        );
+
+        jpMenu.add(jpSettings, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 420, 150, 40));
+
+        getContentPane().add(jpMenu, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 150, 540));
+
+        jSeparator1.setBackground(new java.awt.Color(229, 231, 235));
+        jSeparator1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(229, 231, 235)));
+        getContentPane().add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 0, 2, 540));
+
+        jpView.setBackground(new java.awt.Color(249, 250, 251));
+        jpView.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jlTitleD.setFont(new java.awt.Font("Roboto Black", 1, 18)); // NOI18N
+        jlTitleD.setText("Dashboard");
+        jpView.add(jlTitleD, new org.netbeans.lib.awtextra.AbsoluteConstraints(25, 10, 680, -1));
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 200, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 130, Short.MAX_VALUE)
+        );
+
+        jpView.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(25, 50, 200, 130));
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 200, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 130, Short.MAX_VALUE)
+        );
+
+        jpView.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 50, -1, -1));
+
+        jPanel3.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 200, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 130, Short.MAX_VALUE)
+        );
+
+        jpView.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 50, 200, -1));
+
+        jPanel4.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 320, Short.MAX_VALUE)
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 280, Short.MAX_VALUE)
+        );
+
+        jpView.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 220, 320, 280));
+
+        jPanel5.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
+        jPanel5.setLayout(jPanel5Layout);
+        jPanel5Layout.setHorizontalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 320, Short.MAX_VALUE)
+        );
+        jPanel5Layout.setVerticalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 280, Short.MAX_VALUE)
+        );
+
+        jpView.add(jPanel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(25, 220, 320, 280));
+
+        getContentPane().add(jpView, new org.netbeans.lib.awtextra.AbsoluteConstraints(153, 0, 740, 540));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -78,5 +279,23 @@ public class Home extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanel5;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator3;
+    private javax.swing.JLabel jlTitle;
+    private javax.swing.JLabel jlTitleD;
+    private javax.swing.JPanel jpAlerts;
+    private javax.swing.JPanel jpDashboard;
+    private javax.swing.JPanel jpInventory;
+    private javax.swing.JPanel jpInvoices;
+    private javax.swing.JPanel jpMenu;
+    private javax.swing.JPanel jpReports;
+    private javax.swing.JPanel jpSale;
+    private javax.swing.JPanel jpSettings;
+    private javax.swing.JPanel jpView;
     // End of variables declaration//GEN-END:variables
 }
